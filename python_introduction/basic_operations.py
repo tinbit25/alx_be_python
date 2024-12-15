befac75d57ecafe212c8a01a24b30b5e61632f5f
tinbit25 <tinbitelias25@gmail.com>
2024-12-15 23:00:59 +0300

    first commit

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,13 @@
 # Define the numbers
-num1 = 10
-num2 = 5
+number1 = 10
+number2 = 5
 
 # Perform calculations
-addition = num1 + num2
-subtraction = num1 - num2
-multiplication = num1 * num2
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
 
 # Print the results
-print(f"Addition of {num1} and {num2} is {addition}")
-print(f"Subtraction of {num1} and {num2} is {subtraction}")
-print(f"Multiplication of {num1} and {num2} is {multiplication}")
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
