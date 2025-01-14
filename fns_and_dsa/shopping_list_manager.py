89d81b5ff33f5082a634a9396b9d1dac065f53fa
tinbit25 <tinbitelias25@gmail.com>
2025-01-14 12:24:02 +0300

    adjusting temp

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,18 +9,17 @@ def display_menu():
     print("4. Exit")
 
 def main():
-    shopping_list = []  # Initialize an empty shopping list
+    shopping_list = []  
     while True:
-        display_menu()  # Display the menu
+        display_menu()  
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            # Prompt for and add an item
             item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"'{item}' has been added to the shopping list.")
         elif choice == '2':
-            # Prompt for and remove an item
+        
             item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
@@ -28,7 +27,7 @@ def main():
             else:
                 print(f"'{item}' not found in the shopping list.")
         elif choice == '3':
-            # Display the shopping list
+            
             if shopping_list:
                 print("Current Shopping List:")
                 for idx, item in enumerate(shopping_list, start=1):
